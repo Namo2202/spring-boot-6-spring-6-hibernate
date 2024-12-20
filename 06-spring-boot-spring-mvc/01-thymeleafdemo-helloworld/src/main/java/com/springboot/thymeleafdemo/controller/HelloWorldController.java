a5d8f4b69bbed7c1a54e8fc12a362adc950e24d5
Namo2202 <nafise.mougamadousalime@gmail.com>
2024-12-20 18:09:44 +0100

    Spring MVC Binding Request Params

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -28,6 +29,18 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Yo! "+ theName;
+
+//        add message to the model
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        theName = theName.toUpperCase();
+
+        String result = "Hey my friend from v3! "+ theName;
 
 //        add message to the model
         model.addAttribute("message", result);
