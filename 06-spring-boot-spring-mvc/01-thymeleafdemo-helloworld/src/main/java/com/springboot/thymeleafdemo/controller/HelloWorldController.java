19ade3786b472109476baeda419b8dba17aff867
Namo2202 <nafise.mougamadousalime@gmail.com>
2024-12-20 18:03:30 +0100

    Adding data to Spring MVC Model

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,4 +18,19 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+//    method to read form data and add data to model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+//        read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        theName = theName.toUpperCase();
+
+        String result = "Yo! "+ theName;
+
+//        add message to the model
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
 }
