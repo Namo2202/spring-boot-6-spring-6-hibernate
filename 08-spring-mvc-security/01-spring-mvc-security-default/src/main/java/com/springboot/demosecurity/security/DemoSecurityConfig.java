7328063c2319f76098d0f6db854fb6a5cfd72a6a
Namo2202 <nafise.mougamadousalime@gmail.com>
2024-12-24 15:45:18 +0100

    Spring MVVC Security - Custom login form part 1

--- 08-spring-mvc-security/01-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-mvc-security/01-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,9 +3,12 @@ package com.springboot.demosecurity.security;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -17,4 +20,15 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()).formLogin(form -> form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+
+        return http.build();
+    }
 }
+
+
