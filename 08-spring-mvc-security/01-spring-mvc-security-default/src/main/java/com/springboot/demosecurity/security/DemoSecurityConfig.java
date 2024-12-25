b34bb174fdb3a8e0be7f478bb1d5b2624b54630d
Namo2202 <nafise.mougamadousalime@gmail.com>
2024-12-25 12:38:53 +0100

    Spring MVVC Security - logout

--- 08-spring-mvc-security/01-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-mvc-security/01-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -25,7 +25,9 @@ public class DemoSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .anyRequest().authenticated()).formLogin(form -> form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+                        .anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll())
+                .logout(logout -> logout.permitAll());
 
         return http.build();
     }
