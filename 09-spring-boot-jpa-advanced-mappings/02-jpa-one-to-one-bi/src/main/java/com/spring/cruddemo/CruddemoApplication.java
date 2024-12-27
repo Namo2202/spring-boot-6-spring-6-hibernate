f1e25bc805ad5521ae7f8d9683b4986294964de1
Namo2202 <nafise.mougamadousalime@gmail.com>
2024-12-27 18:55:02 +0100

    JPA/hibernate advanced mappings - one to one bi

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/spring/cruddemo/CruddemoApplication.java
@@ -0,0 +1,70 @@
+package com.spring.cruddemo;
+
+import com.spring.cruddemo.dao.AppDAO;
+import com.spring.cruddemo.entity.Instructor;
+import com.spring.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
+		return runner -> {
+//			createInstructor(appDAO);
+//			findInstructor(appDAO);
+//			deleteInstructor(appDAO);
+			findInstructorDetail(appDAO);
+		};
+	}
+
+	private void findInstructorDetail(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor detail id: "+ theId);
+
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(theId);
+
+		System.out.println("tempInstructor: "+tempInstructorDetail);
+		System.out.println("the associated instructorDetail only: "+tempInstructorDetail.getInstructor());
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId=1;
+		System.out.println("Deleting instructor id: "+theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Done !");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor id: "+ theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: "+tempInstructor);
+		System.out.println("the associated instructorDetail only: "+tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Tasmia", "Tahmid", "namo@gmail.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.google.com", "Drawing");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor: "+tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done!");
+	}
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/spring/cruddemo/dao/AppDAO.java
@@ -0,0 +1,11 @@
+package com.spring.cruddemo.dao;
+
+import com.spring.cruddemo.entity.Instructor;
+import com.spring.cruddemo.entity.InstructorDetail;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+    Instructor findInstructorById(int theId);
+    void deleteInstructorById(int theId);
+    InstructorDetail findInstructorDetailById(int theId);
+}
