0ee3246e66f4e07b4210f13c281048862d90720f
Namo2202 <nafise.mougamadousalime@gmail.com>
2024-12-28 18:53:01 +0100

    JPA/Hibernate advanced mappings - one to many update course

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/spring/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/spring/cruddemo/CruddemoApplication.java
@@ -30,8 +30,23 @@ public class CruddemoApplication {
 //			findInstructorWithCourses(appDAO);
 //			findCoursesForInstructor(appDAO);
 //			findInstructorWithCoursesJoinFetch(appDAO);
-			updateInstructor(appDAO);
+//			updateInstructor(appDAO);
+			updateCourse(appDAO);
 		};
+	}
+
+	private void updateCourse(AppDAO appDAO) {
+		int theId=10;
+
+		System.out.println("Finding course id: "+ theId);
+		Course tempCourse = appDAO.findCourseById(theId);
+
+		System.out.println("Updating course id: "+theId);
+		tempCourse.setTitle("Python");
+
+		appDAO.updateCourse(tempCourse);
+
+		System.out.println("Done !");
 	}
 
 	private void updateInstructor(AppDAO appDAO) {
--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/spring/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/spring/cruddemo/dao/AppDAOImpl.java
@@ -78,4 +78,15 @@ public class AppDAOImpl implements AppDAO {
     public void update(Instructor tempInstructor) {
         entityManager.merge(tempInstructor);
     }
+
+    @Override
+    @Transactional
+    public void updateCourse(Course tempCourse) {
+        entityManager.merge(tempCourse);
+    }
+
+    @Override
+    public Course findCourseById(int theId) {
+        return entityManager.find(Course.class, theId);
+    }
 }
