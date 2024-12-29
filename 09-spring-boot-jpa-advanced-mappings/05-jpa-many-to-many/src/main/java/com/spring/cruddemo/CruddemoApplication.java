889813f15fa7209ca6c93b6fd9d962a8cc469339
Namo2202 <nafise.mougamadousalime@gmail.com>
2024-12-29 16:49:55 +0100

    JPA/Hibernate advanced mappings - many-to-many find course and associated students

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/spring/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/spring/cruddemo/CruddemoApplication.java
@@ -19,8 +19,17 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			createCourseAndStudents(appDAO);
+//			createCourseAndStudents(appDAO);
+			findCourseAndStudents(appDAO);
 		};
+	}
+
+	private void findCourseAndStudents(AppDAO appDAO) {
+		int theId=10;
+		Course tempCourse = appDAO.findCourseAndStudentsByCourseId(theId);
+		System.out.println("loaded course: "+tempCourse);
+		System.out.println("Students: "+tempCourse.getStudents());
+		System.out.println("Done!");
 	}
 
 	private void createCourseAndStudents(AppDAO appDAO) {
